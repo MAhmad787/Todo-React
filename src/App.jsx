0cbc04b9802e26f48b0449d39cfe4af66d1d103a
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-04-09 00:03:52 +0500

    init & Add newItem feature

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems((currentItems) => {
+      return [
+        ...currentItems,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
+    console.log(items);
+  };
+  const deleteItem = () => {
+    console.log('Deleted');
+  };
+  const toggleItem = (id, completed) => {
+    setItems((currentItems) => {
+      return currentItems.map((item) => {
+        if (item.id === id) {
+          item.completed = completed;
+          return { ...item, completed };
+        }
+        return item;
+      });
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {items.map((item) => (
+          <li key={item.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={item.isCompleted}
+                onChange={(e) => toggleItem(item.id, e.target.checked)}
+              />
+              {item.title}
+            </label>
+            <button onClick={deleteItem} className="btn btn-danger">
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
